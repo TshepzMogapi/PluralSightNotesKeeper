befd512a7be87557390970a87b75522d198cf3b3
Tshepiso Mogapi <tshepzmogapi@gmail.com>
2019-01-24 22:05:19 +0200

    add Junit for testing

---
+++ app/src/test/java/com/example/android/pluralsightnoteskeeper/DataManagerTest.java
@@ -0,0 +1,16 @@
+package com.example.android.pluralsightnoteskeeper;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by tshepisomogapi on 2019/01/24.
+ */
+public class DataManagerTest {
+    @Test
+    public void createNewNote() throws Exception {
+
+    }
+
+}
