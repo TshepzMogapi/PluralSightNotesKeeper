545c03e0ded2eb76ff7a323cca3ba321d8005ed6
Tshepiso Mogapi <tshepzmogapi@gmail.com>
2019-01-29 18:58:12 +0200

    verify behavior

--- app/src/androidTest/java/com/example/android/pluralsightnoteskeeper/NoteCreationTest.java
+++ app/src/androidTest/java/com/example/android/pluralsightnoteskeeper/NoteCreationTest.java
@@ -1,25 +1,40 @@
 package com.example.android.pluralsightnoteskeeper;
 
-import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.*;
-import static android.support.test.espresso.matcher.ViewMatchers.*;
-import static android.support.test.espresso.action.ViewActions.*;
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-
-import static org.junit.Assert.*;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withSpinnerText;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static junit.framework.Assert.assertEquals;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
 
 /**
  * Created by tshepisomogapi on 2019/01/28.
  */
 @RunWith(AndroidJUnit4.class)
 public class NoteCreationTest {
+    static  DataManager sDataManager;
+
+    @BeforeClass
+    public static  void classSetUp() throws Exception {
+        sDataManager = DataManager.getInstance();
+    }
 
     @Rule
     public ActivityTestRule<NoteListActivity> mNoteListActivityRule =
@@ -29,12 +44,39 @@ public class NoteCreationTest {
     @Test
     public void createNewNote() {
 
+        final CourseInfo courseInfo = sDataManager.getCourse("java_lang");
+
+        final String noteTitle = "Test note title";
+        final String noteText = "Body of test note";
+
 
         onView(withId(R.id.fab)).perform(click());
-        onView(withId(R.id.text_note_title)).perform(typeText("Test note title"));
-        onView(withId(R.id.text_note_text)).perform(typeText("Body of test note"),
+
+        onView(withId(R.id.spinner_courses)).perform(click());
+
+        onData(allOf(instanceOf(CourseInfo.class), equalTo(courseInfo))).perform(click());
+
+        onView(withId(R.id.spinner_courses)).check(matches(withSpinnerText(
+                containsString(courseInfo.getTitle()))));
+
+
+        onView(withId(R.id.text_note_title)).perform(typeText(noteTitle))
+            .check(matches(withText(containsString(noteTitle))));
+        onView(withId(R.id.text_note_text)).perform(typeText(noteText),
                 closeSoftKeyboard());
+        onView(withId(R.id.text_note_text)).check(matches(withText(containsString(noteText))));
 
 
+
+        pressBack();
+
+
+
+        int noteIndex = sDataManager.getNotes().size() - 1;
+        NoteInfo note = sDataManager.getNotes().get(noteIndex);
+
+        assertEquals(courseInfo, note.getCourse());
+        assertEquals(noteTitle, note.getTitle());
+        assertEquals(noteText, note.getText());
     }
 }
